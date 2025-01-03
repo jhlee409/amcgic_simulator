1960532cc6fa48f37386ab547dfdee217104e4ad
jhlee409 <jhlee409@gmail.com>
2025-01-03 19:17:35 +0900

    17

--- Login.py
+++ Login.py
@@ -28,19 +28,21 @@ position = st.selectbox("Select Position", ["", "Staff", "F1", "F2 ", "R3", "Stu
 password = st.text_input("Password", type="password")
 
 # 입력 검증
-show_login_button = True
-
-# 모든 조건이 충족되면 로그인 버튼 표시
-if show_login_button:
-    if st.button("Login"):
-        if password == "3180":
-            st.session_state['logged_in'] = True
-            st.session_state['name'] = name
-            st.session_state['position'] = position
-            st.success("로그인 성공!")
-            st.rerun()
-        else:
-            st.error("비밀번호가 틀렸습니다")
+if st.button("Login"):
+    if not name.strip():
+        st.error("한글 이름을 입력해 주세요")
+    elif not position.strip():
+        st.error("position을 선택해 주세요")
+    elif not password.strip():
+        st.error("비밀번호를 입력해 주세요")
+    elif password == "3180":
+        st.session_state['logged_in'] = True
+        st.session_state['name'] = name
+        st.session_state['position'] = position
+        st.success("로그인 성공!")
+        st.rerun()
+    else:
+        st.error("비밀번호가 틀렸습니다")
 
 # 로그아웃 버튼
 if "logged_in" in st.session_state and st.session_state['logged_in']:
