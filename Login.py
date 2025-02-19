e8657da2d3e715bb71ccf6ebf873bde118f31641
jhlee409 <jhlee409@gmail.com>
2025-02-19 16:27:15 +0900

    43

--- Login.py
+++ Login.py
@@ -1,33 +1,18 @@
 import streamlit as st
-import requests
-import json
 import firebase_admin
-from firebase_admin import credentials, db, auth
-from datetime import datetime, timezone
-from utils.auth import handle_logout
+from firebase_admin import credentials, storage
+import os
+import tempfile
+from datetime import datetime
+from pytz import timezone
 
-# Firebase 초기화 (아직 초기화되지 않은 경우에만)
-if not firebase_admin._apps:
+st.set_page_config(page_title="amcgic_simulator")
 
-    # Streamlit Secrets에서 Firebase 설정 정보 로드
-    cred = credentials.Certificate({
-        "type": "service_account",
-        "project_id": st.secrets["project_id"],
-        "private_key_id": st.secrets["private_key_id"],
-        "private_key": st.secrets["private_key"].replace('\\n', '\n'),
-        "client_email": st.secrets["client_email"],
-        "client_id": st.secrets["client_id"],
-        "auth_uri": st.secrets["auth_uri"],
-        "token_uri": st.secrets["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-        "universe_domain": st.secrets["universe_domain"]
-    })
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': st.secrets["FIREBASE_DATABASE_URL"]
-    })
-
-st.set_page_config(page_title="amcgic_education")
+# 세션 상태 초기화
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+    st.session_state['name'] = ''
+    st.session_state['position'] = ''
 
 # Streamlit 페이지 설정
 st.title("AMC GI 상부 Simulator training")
@@ -36,106 +21,42 @@ st.markdown(
     '''
     1. 이 게시판은 서울 아산병원 GI 상부 전용 게시판입니다.
     1. GI 상부의 simulattor training 관련 자료를 제곻하고 결과를 업로드하기 위한 페이지 입니다.
-    1. 한글 이름은 게시판에 접속하셨는지 확인하는 자료이므로 반드시 기입해 주세요..
+    1. 한글 이름은 게시판에 접속하셨는지 확인하는 자료이므로 반드시 기입해 주세요.
     '''
 )
 st.divider()
-# 한글 이름 확인 함수
-def is_korean_name(name):
-    return any('\u3131' <= char <= '\u3163' or '\uac00' <= char <= '\ud7a3' for char in name)
 
-# 사용자 인풋
-email = st.text_input("Email")
-password = st.text_input("Password", type="password")
-name = st.text_input("Name")  # 이름 입력 필드 추가
-position = st.selectbox("Select Position", ["", "Staff", "F1", "F2", "R3", "Student"])  # 직책 선택 필드 추가
+if st.session_state['logged_in']:
+    st.success(f"{st.session_state['name']}님, 환영합니다. 왼쪽 메뉴에서 원하시는 항목을 선택해주세요.")
+    if st.sidebar.button("Logout"):
+        st.session_state['logged_in'] = False
+        st.session_state['name'] = ''
+        st.session_state['position'] = ''
+else:
+    # 사용자 입력
+    name = st.text_input("Your Name (예: 홍길동)")
+    position = st.selectbox("Select Position", ["", "Staff", "F1", "F2 ", "R3", "Student"])
+    password = st.text_input("Password", type="password")
 
-# 유효성 검사 및 로그인 버튼
-login_disabled = False
-if position == "":
-    st.error("position을 선택해 주세요")
-    login_disabled = True
-if not name:
-    st.error("한글 이름을 입력해 주세요")
-    login_disabled = True
-elif not name or not is_korean_name(name):
-    st.error("한글 이름을 입력해 주세요")
-    login_disabled = True
-
-def handle_login(email, password, name, position):
-    try:
-        # Streamlit secret에서 Firebase API 키 가져오기
-        api_key = st.secrets["FIREBASE_API_KEY"]
-        request_ref = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
-        headers = {"Content-Type": "application/json"}
-        data = json.dumps({"email": email, "password": password, "returnSecureToken": True})
-
-        response = requests.post(request_ref, headers=headers, data=data)
-        response_data = response.json()
-
-        if response.status_code == 200:
-            # Firebase Authentication 성공 후 사용자 정보 가져오기
-            user_id = response_data['localId']
-            id_token = response_data['idToken']  # ID 토큰 저장
-            
-            # Authentication 사용자 정보 업데이트
-            try:
-                user = auth.update_user(
-                    user_id,
-                    display_name=name,
-                    custom_claims={'position': position}
-                )
-            except auth.UserNotFoundError:
-                # 사용자가 없는 경우, 새로운 사용자 생성
-                try:
-                    user = auth.create_user(
-                        uid=user_id,
-                        email=email,
-                        password=password,
-                        display_name=name
-                    )
-                    # 생성된 사용자에 대한 custom claims 설정
-                    auth.set_custom_user_claims(user_id, {'position': position})
-                    st.success("새로운 사용자가 생성되었습니다.")
-                except Exception as e:
-                    st.error(f"사용자 생성 중 오류 발생: {str(e)}")
-                    return
-            
-            # Realtime Database에도 정보 저장
-            user_ref = db.reference(f'users/{user_id}')
-            user_data = user_ref.get()
-
-            if user_data is None:
-                # 새 사용자인 경우 정보 저장
-                user_ref.set({
-                    'email': email,
-                    'name': name,
-                    'position': position,
-                    'created_at': {'.sv': 'timestamp'}  # 서버 타임스탬프 사용
-                })
-                user_data = {'name': name, 'position': position}
-            
-            # position이 없는 경우 업데이트
-            elif 'position' not in user_data:
-                user_ref.update({
-                    'position': position
-                })
-                user_data['position'] = position
-
+    # 로그인 버튼
+    if st.button("Login"):
+        if not name.strip():
+            st.error("한글 이름을 입력해 주세요")
+        elif not position.strip():
+            st.error("position을 선택해 주세요")
+        elif not password.strip():
+            st.error("비밀번호를 입력해 주세요")
+        elif password == "3180":
             st.session_state['logged_in'] = True
-            st.session_state['user_email'] = email
-            st.session_state['user_name'] = name
-            st.session_state['user_position'] = position
-            st.session_state['user_id'] = user_id
+            st.session_state['name'] = name
+            st.session_state['position'] = position
+            st.success("로그인 성공!")
+
+            # 로그아웃 버튼
+            if "logged_in" in st.session_state and st.session_state['logged_in']:
+                if st.sidebar.button("Logout"):
+                    st.session_state['logged_in'] = False
+                    st.success("로그아웃 되었습니다.")
+
         else:
-            st.error(response_data["error"]["message"])
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
-
-if st.button("Login", disabled=login_disabled):
-    handle_login(email, password, name, position)
-
-# 로그아웃 버튼
-if "logged_in" in st.session_state and st.session_state['logged_in']:
-    st.success(f"로그인 성공")    
-    handle_logout()
+            st.error("비밀번호가 틀렸습니다")
